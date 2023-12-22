8ff2f75047da045385243ca4dd26e0ad1e3261f7
Yatharth <yatharth0903@gmail.com>
2023-12-22 14:15:12 +0530

    week-3

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const zod=require("zod");
 
 /**
  * Generates a JWT for a given username and password.
@@ -13,8 +13,19 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+
+const emailSchema=zod.string().email();
+const passwordSchema=zod.string().min(6);
+
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse=emailSchema.safeParse(username);
+    const passwordResponse=passwordSchema.safeParse(password);
+
+    if(!usernameResponse.success || !passwordResponse.success) return null;
+
+    const signature=jwt.sign({username},jwtPassword);
+    return signature;
 }
 
 /**
@@ -26,7 +37,15 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    let ans=true;
+    try{
+        jwt.verify(token,jwtPassword) // this will not retun null same as decode but will retun error that's why wrapped in try catch
+    }
+    catch(e){
+        ans=false;
+    }
+    return ans;
+    
 }
 
 /**
@@ -37,7 +56,10 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+
+    const decoded=jwt.decode(token)
+    if(decoded) return true;
+    else return false;
 }
 
 
